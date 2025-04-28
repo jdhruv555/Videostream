1090f68b00c448c80c605601b53c47b62d797d0a
Dhruv <135132021+jdhruv555@users.noreply.github.com>
2025-04-29 00:06:20 +0530

    Initial commit: Video streaming application

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,8 +7,8 @@ const Footer: NextPage = () => {
     return (
         <footer className='h-20 z-10 w-screen bg-transparent flex justify-center items-center px-10'>
             <nav className='flex justify-between items-start w-full h-full'>
-                <span className='text-2xl md:text-3xl font-bold text-slate-50 hover:text-neutral-300 cursor-pointer'>TANISHQ SINGH</span>
-                <span className='text-2xl md:text-3xl font-bold text-slate-50 hover:text-neutral-300 cursor-pointer'>CONTACT US</span>
+                <span className='text-2xl md:text-3xl font-bold text-slate-50 hover:text-neutral-300 cursor-pointer'>Dhruv Jha </span>
+                <span className='text-2xl md:text-3xl font-bold text-slate-50 hover:text-neutral-300 cursor-pointer'></span>
             </nav>
         </footer>
     );
